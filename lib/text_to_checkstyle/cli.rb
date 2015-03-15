5a0551da8fe7d55e53c3ecf62ac991f93b544c84
sanemat <o.gata.ken@gmail.com>
2015-03-16 06:05:40 +0900

    chore(cli): show input data

--- lib/text_to_checkstyle/cli.rb
+++ lib/text_to_checkstyle/cli.rb
@@ -32,6 +32,7 @@ module TextToCheckstyle
           end
 
       fail(NoInputError) if !data || data.empty?
+      logger.info('data: ' + data)
     rescue StandardError => e
       suggest_messages(options)
       raise e
