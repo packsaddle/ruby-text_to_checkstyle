5d97e9478430b3f312b3dbd72020b3b5fc98f12c
sanemat <o.gata.ken@gmail.com>
2015-03-17 02:09:58 +0900

    test(convert): valid text

--- test/test_converter.rb
+++ test/test_converter.rb
@@ -2,10 +2,26 @@ require_relative 'helper'
 
 module TextToCheckstyle
   class TestConverter < Test::Unit::TestCase
-    test '.convert' do
-      text = ''
-      assert_raise(NoInputError) do
-        Converter.convert(text)
+    sub_test_case '.convert' do
+      test 'empty text' do
+        text = ''
+        assert_raise(NoInputError) do
+          Converter.convert(text)
+        end
+      end
+      test 'valid text' do
+        text = 'valid text'
+        expected = <<-EOS.gsub(/[\n\r]/) { '' }.gsub(/[ ]{2,}/) { '' }
+          <?xml version='1.0'?>
+          <checkstyle>
+            <file name='path/to/file'>
+              <error column='0' line='0' message='valid text' severity='info' source='TextToCheckstyle'/>
+            </file>
+          </checkstyle>
+        EOS
+        assert do
+          Converter.convert(text) == expected
+        end
       end
     end
   end
